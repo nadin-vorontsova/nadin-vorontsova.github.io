7358dd27bf7f218510ba83485e367b2484698427
nadin-vorontsova <nadin.countess@gmail.com>
2022-01-17 11:56:39 +0300

    post Fethie

--- compress_pictures.bat
+++ compress_pictures.bat
@@ -1,7 +1,7 @@
 echo off
 set /p directory="Enter directory path with pictures you want to compress: "
 echo Compressing pictures in directory %directory%...
-magick mogrify -resize 2000x -quality 85 "%directory%\*.jpg"
+magick mogrify -strip -interlace Plane -gaussian-blur 0.05 -quality 85  "%directory%\*.*"
 echo Done.
 timeout 2 >NUL
 exit
